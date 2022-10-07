94d27ed3c7ce56fc65b7570ad04ed4afe11186a4
affvini <vinicius.f.veras@gmail.com>
2022-10-07 11:15:37 -0300

    update code

--- Aula07/src/Lutador/Lutador.java
+++ Aula07/src/Lutador/Lutador.java
@@ -12,6 +12,7 @@ public class Lutador {
         private int empates;
 
         public void apresentar(){
+            System.out.println("------------------------------------");
             System.out.println("Lutador: " + this.getNome());
             System.out.println("Origem: " + this.getNacionalidade());
             System.out.println(this.getIdade() + " Anos");
@@ -23,19 +24,19 @@ public class Lutador {
         }
 
         public void status() {
-            System.out.println(this.getNome());
-            System.out.println("É um peso " + this.getCategoria());
+            System.out.print(this.getNome());
+            System.out.println(" é um peso " + this.getCategoria());
             System.out.println(this.getVitorias() + " vitórias");
             System.out.println(this.getDerrotas() + " derrotas");
             System.out.println(this.getEmpates() + " empates");
         }
 
         public void ganharLuta() {
-            this.setVitorias(this.getVitorias() + 1);
+            this.setVitorias(getVitorias() + 1);
         }
 
         public void perderLuta() {
-            this.setVitorias(this.getVitorias() - 1);
+            this.setDerrotas(this.getDerrotas() + 1);
         }
 
         public void empatarLuta() {
@@ -90,24 +91,25 @@ public class Lutador {
     }
 
     public void setPeso(double peso) {
-        this.peso = peso;
+            this.peso = peso;
+            this.setCategoria();
     }
 
     public String getCategoria() {
         return categoria;
     }
 
-    public void setCategoria(String categoria) {
-        if (this.getPeso() < 52.2){
-           categoria = "Invalido";
-        } else if (this.getPeso() <= 70.3){
-            categoria = "Leve";
-        } else if (this.getPeso() <= 83.9){
-            categoria = "Médio";
-        } else if (this.getPeso() <= 120.0){
-            categoria = "Pesado";
+    private void setCategoria() {
+        if (this.peso < 52.2){
+           this.categoria = "Invalido";
+        } else if (this.peso <= 70.3){
+            this.categoria = "Leve";
+        } else if (this.peso <= 83.9){
+            this.categoria = "Médio";
+        } else if (this.peso <= 120.0){
+            this.categoria = "Pesado";
         } else {
-            categoria = "Inválido";
+            this.categoria = "Inválido";
         }
     }
 
--- Aula07/src/Main.java
+++ Aula07/src/Main.java
@@ -1,5 +1,23 @@
+import Lutador.Lutador;
+
 public class Main {
     public static void main(String[] args) {
-        L = vector[0,5];
+       Lutador l[] = new Lutador[6];
+       l[0] = new Lutador("Pretty Boy", "França", 31,
+               1.75,68.9,11, 2, 1);
+       l[1] = new Lutador ("Putscript", "Brasil", 29,
+               1.68,57.8,14,2,3);
+       l[2] = new Lutador("Snapshadow", "EUA", 35,1.65,
+               80.9,12,2,1);
+       l[3] = new Lutador("Dead Code", "Austrália", 28,1.93,
+               81.6,13,0,2);
+       l[4] = new Lutador("UFOCobol", "Brasil", 37,1.70,
+               119.3,5,4,3);
+       l[5] = new Lutador("Nerdaart", "EUA", 30,1.81,
+               105.7,12,2,4);
+
+       l[3].perderLuta();
+       l[3].empatarLuta();
+       l[3].status();
     }
 }
